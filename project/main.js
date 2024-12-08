201bf31399158aeba792b000a677402f83201c08
Nakul Mehra <nakulmehra0498@gmail.com>
2024-12-08 10:47:35 +0530

    first web commit

---
+++ project/main.js
@@ -0,0 +1,43 @@
+console.log('Hello World..');
+/* To print we can use 
+    1. console.log
+    2. console.error
+    3. Alert
+    */
+console.error('this message is error');
+alert('how can you forget Allert'); 
+let a=100;
+console.log(a);
+var b=101;
+console.log(b);
+const c=102;
+console.log(c);
+// let d;
+// d=100;
+// above will work but now with const
+// const e;
+// e=100;
+
+
+/* datatypes in js
+every thing in js is an object
+there are 3 ways to declare a datatype
+1. let
+    eg. let a=100;
+    {
+        let a
+    }
+    let - creates a local variable we will not be able to use it outside a block of code     
+2. var
+    eg. var b=101;
+    {
+        var a
+    }
+    var - creates a glocal variable, that can also be used outside the block, it has global scope.    
+3. const
+    eg. const c =102;
+    */
+// typeof gives the data type of a variable 
+// const a = 100;
+// console.log(typeof a); -> number
+// we have 1. number, 2.string , 3.null in js as datatype 4.undefined
